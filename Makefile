323d3dde25c34e139079c8226e5f3ac50bbd23d0
Jennifer Liu <jenniferliu1272@gmail.com>
2023-07-08 09:37:36 +0800

    Change to using [] brackets

--- Makefile
+++ Makefile
@@ -13,7 +13,6 @@ mini-playground:
 	@echo "Running playground..."
 	@go run ./playground/main.go
 
-# Using a crude way to start a server. This is not a recommended approach 
 # Usage: make start 
 start:
 	@echo "Starting server..."
--- actions.go
+++ actions.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func listAction(repoName string, userName string) interface{} {
 	if !usernameExist(userName) {
-		return fmt.Sprintf("Username: %v does not exist. Please provide the username using `/pr list <repo-name> <github-username>` command", userName)
+		return fmt.Sprintf("Username: %v does not exist. Please provide the username using `/pr list [repo-name] [github-username]` command", userName)
 	}
 
 	return listPRsMessage(userName, repoName)
